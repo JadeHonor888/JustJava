d9bfcfd2de1ca8ef6675b88ed2ddc6e2dca629d1
JadeL <jade.libson@gmail.com>
2022-09-16 12:57:33 -0500

    Update w/ scroll and check boxes: initial commit

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
@@ -18,12 +19,25 @@ public class MainActivity extends AppCompatActivity {
     }
 
     int numOfCoffees = 0;
+    boolean hasWhippedCream = false;
+    boolean hasChocolate = false;
+    String name;
     //when the button is clicked
     public void submitOrder(View view)
     {
         display(numOfCoffees);
         displayPrice(numOfCoffees * 5);
     }
+
+    public void getName(View view)
+    {
+        EditText enterName = (EditText) findViewById(R.id.enterName);
+        //enterName.setOnClickListener(findViewById(R.id.enterName));
+        name = enterName.getText().toString();
+    }
+
+    public void toppings1(View view) {hasWhippedCream = !hasWhippedCream;}
+    public void toppings2(View view) {hasChocolate = !hasChocolate;}
 
     public void add(View view)
     {
@@ -59,7 +73,17 @@ public class MainActivity extends AppCompatActivity {
     private void displayPrice(int num)
     {
         TextView cost = (TextView) findViewById(R.id.cost);
-        cost.setText("Total: " + NumberFormat.getCurrencyInstance().format(num) +
-                    "\nThank you!");
+        String text =   "Name: " + name +
+                        "\nTotal: " + NumberFormat.getCurrencyInstance().format(num) +
+                        "\nThank you!";
+        if (hasChocolate)
+        {
+            text = "Has chocolate. \n" + text;
+        }
+        if (hasWhippedCream)
+        {
+            text = "Has whipped cream. \n" + text;
+        }
+        cost.setText(text);
     }
 }
